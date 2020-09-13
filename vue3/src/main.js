f0175ac9a8a87eb3ca94c3a776ab97004660378d
xiziliang <54427136+xiziliang@users.noreply.github.com>
2020-09-13 09:11:33 +0800

    初始化

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+let app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
