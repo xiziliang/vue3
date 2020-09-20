1ffed52278138ebaccdf384b4af68313b3f1a9d1
xiziliang <54427136+xiziliang@users.noreply.github.com>
2020-09-20 17:48:00 +0800

    feat: vue3 API 练习(1)

--- vue3/src/views/About.vue
+++ vue3/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div ref="root">{{abc}}</div>
 </template>
+
+<script>
+import {
+  ref,
+  onMounted
+} from 'vue'
+export default {
+  setup(props, context) {
+    const root = ref();
+    console.log(context)
+    onMounted(() => {
+      console.log(root);
+    })
+    return {
+      root,
+      abc: ref('今晚打老虎')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
