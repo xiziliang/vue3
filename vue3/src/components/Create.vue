9f717b66bf8ba9da4427afd90f499f1e2dc425ce
ZR19110124-席子良 <xiziliang@zhongruigroup.com>
2020-11-09 09:08:04 +0800

    feat: 添加create组件

---
+++ vue3/src/components/Create.vue
@@ -0,0 +1,16 @@
+<template>
+<div>creat组件</div>
+</template>
+
+<script>
+export default {
+    name: "Create",
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style  scoped>
+</style>
