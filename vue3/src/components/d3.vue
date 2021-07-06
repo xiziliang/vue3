989f4ed448604418aef42416446e431f753d4f44
席子良 <ziliang.xi@eoitek.com>
2021-07-06 18:14:53 +0800

    feat: 添加d3

---
+++ vue3/src/components/d3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box"></div>
+</template>
+<script>
+import { ref, reactive, toRefs, computed, watch, onMounted } from "vue";
+import d3 from "d3";
+function init() {
+  console.log(d3.select(".box"));
+}
+export default {
+  name: "D3",
+  setup(prop) {
+    onMounted(() => {
+      init();
+    });
+    return {};
+  },
+};
+</script>
+
--- vue3/src/views/Home.vue
+++ vue3/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld/>
+    <D3/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '../components/HelloWorld.vue'
+import D3 from '../components/d3.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    D3
   }
 }
 </script>
